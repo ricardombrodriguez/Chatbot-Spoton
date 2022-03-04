4839e8674c7dad2f24f0eb5785f9815de894bd1f
joaoreis16 <joaoreis16@ua.pt>
2022-03-04 19:09:38 +0000

    tamos fodidos, siga beber fino

--- backend/spoton/app/views.py
+++ backend/spoton/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+
 
 # Create your views here.
+get_fligths:
+
+    request api (/flights)
