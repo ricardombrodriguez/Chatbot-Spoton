5e2fc31f8020f43067690465b4894edd89329837
ricardombrodriguez <ricardodrodriguez@ua.pt>
2022-03-04 22:49:19 +0000

    frontend

---
+++ frontend/app/src/app/services/chatbot.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatbotService {
+
+  baseUrl = 'http://127.0.0.1:8000/';
+
+  constructor(private http: HttpClient) { }
+
+  sendMessage(message:string): Observable<any> {
+    return this.http.get<any>(this.baseUrl + 'message?msg' + message);
+  }
+
+}
