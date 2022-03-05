c447615767f09f66aa5f5330e77dfa583f817155
PauloGSP <paulogspereira@ua.pt>
2022-03-05 01:56:18 +0000

    tava a ver q n

--- backend/spoton/app/NLP.py
+++ backend/spoton/app/NLP.py
@@ -1,11 +1,11 @@
-import nltk
 
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.wordnet import WordNetLemmatizer
 
-stop_words = ['the', 'you', 'i', 'are', 'is', 'a', 'me', 'to', 'can', 'this', 'your', 'have', 'any', 'of', 'we', 'very',
-              'could', 'please', 'it', 'with', 'here', 'if', 'my', 'am']
+stop_words = ['the', 'you', 'i', 'are', 'is', 'a', 'me', 'can', 'this', 'your', 'have', 'any', 'of', 'we', 'very',
+              'could', 'please', 'it', 'with', 'here', 'if', 'my', 'am', 'what']
 
+keywords = ['to','from','cheap','low-cost','affordable','economic','low-price','low','price','cost','economical']
 
 def lemmatize_sentence(tokens):
     lemmatizer = WordNetLemmatizer()
@@ -18,6 +18,13 @@ def tokenize_and_remove_punctuation(sentence):
     tokens = tokenizer.tokenize(sentence)
     return tokens
 
+#TO IMPLEMENT
+def findkeys(word_tokens):
+    kws=[]
+    for w in word_tokens:
+        if w in keywords:
+            kws.append(w)
+    return kws
 
 def remove_stopwords(word_tokens):
     filtered_tokens = []
@@ -27,21 +34,24 @@ def remove_stopwords(word_tokens):
     return filtered_tokens
 
 
-'''
-Convert to lower case,
-remove punctuation
-lemmatize
-'''
-
-
 def preprocess_main(sent):
-    sent = sent.lower()
-    tokens = tokenize_and_remove_punctuation(sent)
-    lemmatized_tokens = lemmatize_sentence(tokens)
-    orig = lemmatized_tokens
-    filtered_tokens = remove_stopwords(lemmatized_tokens)
-    if len(filtered_tokens) == 0:
+    sent = sent.lower() # lowercase
+    tokens = tokenize_and_remove_punctuation(sent) # transform input/message in tokens (separated terms)
+    lemmatized_tokens = lemmatize_sentence(tokens) # group similar tokens to a specific one
+    orig = lemmatized_tokens  
+
+    filtered_tokens = remove_stopwords(lemmatized_tokens)   # remove irrelevant words
+    if len(filtered_tokens) == 0:   
         # if stop word removal removes everything, don't do it
         filtered_tokens = orig
     normalized_sent = " ".join(filtered_tokens)
     return normalized_sent
+
+
+if __name__ == '__main__':
+    print(preprocess_main("i want a flight to amsterdam"))
+    print(preprocess_main("what can you do?"))
+    print(preprocess_main("show me all airports"))
+    print(preprocess_main("i want cheap flights from porto to tokyo"))
+
+    
